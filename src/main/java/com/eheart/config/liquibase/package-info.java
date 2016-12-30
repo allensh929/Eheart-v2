4469ac65f1abcc5a092cc299d0c1939cfe76197e
Chen Li <chen.li@blackboard.com>
2016-12-31 00:35:48 +0800

    init eheart v2

---
+++ src/main/java/com/eheart/config/liquibase/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Liquibase specific code.
+ */
+package com.eheart.config.liquibase;
---
+++ src/main/java/com/eheart/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data ElasticSearch repositories.
+ */
+package com.eheart.repository.search;
